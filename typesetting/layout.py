ec2e1ef0e11e62419cbea04f2542f281f5146c07
Rich Wareham <rich@richwareham.com>
2023-08-26 14:30:52 +0100

    futher move to typesetting module

--- typesetting/layout.py
+++ typesetting/layout.py
@@ -1,12 +1,13 @@
 import dataclasses
 import enum
-import typing
+from collections import namedtuple
+from typing import TYPE_CHECKING, Generator, Iterable
 
 import uniseg.linebreak
 
 from .hyphenation import SOFT_HYPHEN
 
-if typing.TYPE_CHECKING:
+if TYPE_CHECKING:
     from .font import Font
 
 __all__ = [
@@ -14,6 +15,8 @@ __all__ = [
     "ParagraphItem",
     "text_to_paragraph_items",
     "MAX_PENALTY",
+    "paragraph_item_running_sums",
+    "greedy_line_breaks",
 ]
 
 
@@ -42,9 +45,7 @@ def _text_width(text: str, font: "Font") -> float:
     return sum(g.x_advance for g in font.shape(text))
 
 
-def text_to_paragraph_items(
-    text: str, font: "Font"
-) -> typing.Generator[ParagraphItem, None, None]:
+def text_to_paragraph_items(text: str, font: "Font") -> Generator[ParagraphItem, None, None]:
     space_width = _text_width(" ", font)
     hyphen_width = _text_width("-", font)
 
@@ -90,3 +91,53 @@ def text_to_paragraph_items(
         penalty=-MAX_PENALTY,
         flagged=True,
     )
+
+
+RunningSum = namedtuple("RunningSum", ["width", "shrinkability", "stretchability"])
+
+
+def paragraph_item_running_sums(
+    para_items: Iterable[ParagraphItem],
+) -> Generator[RunningSum, None, None]:
+    previous_sum = RunningSum(0, 0, 0)
+    yield previous_sum
+    for item in para_items:
+        if item.item_type != ParagraphItemType.PENALTY:
+            previous_sum = RunningSum(
+                width=previous_sum.width + item.width,
+                shrinkability=previous_sum.shrinkability + item.shrinkability,
+                stretchability=previous_sum.stretchability + item.stretchability,
+            )
+        yield previous_sum
+
+
+def greedy_line_breaks(
+    para_items: Iterable[ParagraphItem], width: float
+) -> Generator[int, None, None]:
+    "Return sequence of indices in para_items for line breaks."
+    sums = list(paragraph_item_running_sums(para_items))
+    current_start_idx = 0
+    for item_idx, item in enumerate(para_items):
+        # Don't break lines at boxes.
+        if item.item_type == ParagraphItemType.BOX:
+            continue
+
+        # Forced break
+        if item.penalty <= -MAX_PENALTY:
+            yield item_idx
+            current_start_idx = item_idx + 1
+            continue
+
+        # Compute natural width and total stretch/shrinkability
+        line_width = sums[item_idx].width - sums[current_start_idx].width
+        if item.item_type == ParagraphItemType.PENALTY:
+            line_width += item.width
+        line_stretchability = (
+            sums[item_idx].stretchability - sums[current_start_idx].stretchability
+        )
+
+        if line_width + line_stretchability < width:
+            continue
+
+        yield item_idx
+        current_start_idx = item_idx + 1
